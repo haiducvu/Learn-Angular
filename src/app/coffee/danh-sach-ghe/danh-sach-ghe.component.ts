6e2528fdd325ffefc975e2c10ac92285a3a430df
Hai Vu <duchaivu1997@gmail.com>
2021-04-13 01:13:03 +0700

    show ghe da duoc chon

--- src/app/coffee/danh-sach-ghe/danh-sach-ghe.component.ts
+++ src/app/coffee/danh-sach-ghe/danh-sach-ghe.component.ts
@@ -9,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class DanhSachGheComponent implements OnInit {
   DanhSachGhe= DanhSachGhe;  
   soGheDaDat:number=0;
-  soGheConLai:number;
+  soGheConLai:number=0;
   DanhSachGheDangDat=[];
   constructor() {
     
   }
 
   ngOnInit(): void {
-    this.soGheConLai=this.DanhSachGhe.length;
+    // this.soGheConLai=this.DanhSachGhe.length;
+    for(let ghe of DanhSachGhe){
+        if(!ghe.TrangThai){
+         this.soGheConLai++
+        }
+    }
   }
   DatGheParent(status, ghe){
     if(status){
